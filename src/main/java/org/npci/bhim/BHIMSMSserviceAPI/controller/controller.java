37b0e4ad22efe9c9d23271b547972604e3691a58
saipavanreddy <saipavanreddy3105@gmail.com>
2025-09-02 19:07:08 +0530

    added some pojos. Designed new api request , consent details

--- src/main/java/org/npci/bhim/BHIMSMSserviceAPI/controller/controller.java
+++ src/main/java/org/npci/bhim/BHIMSMSserviceAPI/controller/controller.java
@@ -2,6 +2,8 @@ package org.npci.bhim.BHIMSMSserviceAPI.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.npci.bhim.BHIMSMSserviceAPI.model.Consent;
 import org.npci.bhim.BHIMSMSserviceAPI.model.GetConsentData;
 import org.npci.bhim.BHIMSMSserviceAPI.model.Registration;
@@ -9,7 +11,6 @@ import org.npci.bhim.BHIMSMSserviceAPI.model.TextMsgRequest;
 import org.npci.bhim.BHIMSMSserviceAPI.service.AuthenticateService;
 import org.npci.bhim.BHIMSMSserviceAPI.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
@@ -18,7 +19,16 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/whatsapp")
+@Slf4j
 public class controller {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @PostConstruct
+    public void logObjectMapperModules() {
+        objectMapper.getRegisteredModuleIds().forEach(id -> System.out.println("Registered module: " + id));
+    }
 
     @Autowired
     private AuthenticateService authenticateService;
@@ -47,8 +57,6 @@ public class controller {
 //        ObjectMapper mapper=new ObjectMapper();
 //        System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(request));
 //
-
-
         return authenticateService.optInRequest(request);
     }
 
@@ -66,8 +74,11 @@ public class controller {
     @GetMapping("/getoptin")
     public Mono<byte[]> sedMessage(@RequestBody GetConsentData request) throws JsonProcessingException {
 
-//        ObjectMapper mapper=new ObjectMapper();
-//        System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(request));
+        ObjectMapper mapper=new ObjectMapper();
+        log.info(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(request));
+//        LocalDate from = LocalDate.parse(request.getFromDate());
+//        LocalDate to = LocalDate.parse(request.getToDate());
+
         return authenticateService.getConsent(request);
     }
 
--- src/main/java/org/npci/bhim/BHIMSMSserviceAPI/model/GetConsentData.java
+++ src/main/java/org/npci/bhim/BHIMSMSserviceAPI/model/GetConsentData.java
@@ -1,12 +1,16 @@
 package org.npci.bhim.BHIMSMSserviceAPI.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class GetConsentData {
 
     private LocalDate fromDate;
