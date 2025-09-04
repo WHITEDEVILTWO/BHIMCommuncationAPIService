5c112e78f11496c88b22118b6bfe30dcd47b4f6e
saipavanreddy <saipavanreddy3105@gmail.com>
2025-09-04 18:44:29 +0530

    redis token manager service added, mobile number validation added

--- src/main/java/org/npci/bhim/BHIMSMSserviceAPI/controller/CallBackController.java
+++ src/main/java/org/npci/bhim/BHIMSMSserviceAPI/controller/CallBackController.java
@@ -2,6 +2,7 @@ package org.npci.bhim.BHIMSMSserviceAPI.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 @RestController
 @RequestMapping("/waAPICallBack/sinch")
 @Slf4j
+@EnableAsync
 public class CallBackController {
 
     private static final List<Map<String,Object>> callbackList=new CopyOnWriteArrayList<>();
