dfbe0d4c525a6d9079965e7a6f15d69268da6e19
saipavanreddy <ganesh.repalle949@gmail.com>
2025-09-12 13:46:33 +0530

    CallBac Controller is added

--- src/main/java/org/npci/bhim/BHIMSMSserviceAPI/controller/CallBackController.java
+++ src/main/java/org/npci/bhim/BHIMSMSserviceAPI/controller/CallBackController.java
@@ -2,7 +2,6 @@ package org.npci.bhim.BHIMSMSserviceAPI.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,20 +9,19 @@ import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 @RestController
-@RequestMapping("/callback/sinch")
-@Slf4j
+@RequestMapping("/communication/callback")
 public class CallBackController {
 
     private static final List<Map<String,Object>> callbackList=new CopyOnWriteArrayList<>();
-    @PostMapping
-    public ResponseEntity<Void> receiveCallback(@RequestBody Map<String,Object> payload){
-        callbackList.add(payload);
-        log.info("Received Sich Call back: {}",payload);
-        return ResponseEntity.ok().build();
-    }
 
-    @GetMapping("/all")
-    public ResponseEntity<List<Map<String,Object>>> getCallBacks(){
-        return ResponseEntity.ok(callbackList);
+    @PostMapping("/wa")
+    public void receiveCallback(@RequestBody Map<String,Object> payload){
+        //our logic goes here
+        //database pushing
+    }
+    @PostMapping("/rcs")
+    public void receiveCallBackRCS(@RequestBody Map<String,Object> payload){
+        //our logic goes here
+        //database pushing
     }
 }
